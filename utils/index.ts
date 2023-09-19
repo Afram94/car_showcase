dd2552cb595b19a6bbf8e8a71762012875e1a5e4
Afram94 <afram.h@hotmail.com>
2023-09-19 22:37:35 +0200

    Done with the search using searchParams

--- utils/index.ts
+++ utils/index.ts
@@ -1,12 +1,15 @@
-import { CarProps } from "@/types";
+import { CarProps, FilterProps } from "@/types";
 
-export async function fetchCars() {
+export async function fetchCars(filters: FilterProps) {
+
+    const {manufacturer, year, fuel, limit, model} = filters;
+
     const headers = {
         'X-RapidAPI-Key': 'e09e5d97a5msh384afaaa346a7b3p1a36b4jsn217a7181e693',
         'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
         }
 
-        const response = await fetch( 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=q3',{
+        const response = await fetch(`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,{
             headers: headers,
         });
 
